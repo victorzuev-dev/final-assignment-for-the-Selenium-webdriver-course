cd94206ae5d685c2aa12678f2aa8921522871104
Victor <victorzuev87@outlook.com>
2023-07-10 10:20:08 +0300

    added a link search test to the login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,3 @@
-# run test: pytest -v --tb=line --language=en test_main_page.py
-
 from pages.main_page import MainPage
 
 
@@ -8,3 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
