557d9d1ef5a4918e4e68b7c86ae26cd0f65c555c
Victor <victorzuev87@outlook.com>
2023-07-13 09:09:02 -0500

    added locator class for product page

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,10 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_REGISTER = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    ALERT_INNER_PRODUCT_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_page .product_main h1")
+    ALERT_INFO_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner p strong")
+    PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
