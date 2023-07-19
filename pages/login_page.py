a0ff25fdeb71a2e2b97fd395111e71f1d4b653c9
Victor <victorzuev87@outlook.com>
2023-07-19 07:44:58 -0500

    new user registration added

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,8 @@
+from selenium.webdriver.common.by import By
+
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import LoginPageLocators, BasePageLocators
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -17,3 +20,18 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        browser = self.browser
+        email_field = browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_field_1 = browser.find_element(*LoginPageLocators.PASSWORD_FIELD_1)
+        password_field_1.send_keys(password)
+        password_field_2 = browser.find_element(*LoginPageLocators.PASSWORD_FIELD_2)
+        password_field_2.send_keys(password)
+        button_registration = browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION)
+        button_registration.click()
+        assert browser.find_element(*BasePageLocators.SUCCESS_MESSAGE_REGISTRATION), "Fail registration"
+
+
+
