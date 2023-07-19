66a4d3e8e1bac0a70a865640a8b612eb85da9113
Victor <victorzuev87@outlook.com>
2023-07-19 07:47:52 -0500

    added check for disappearance of the message about successful adding an item to cart

--- pages/product_page.py
+++ pages/product_page.py
@@ -22,5 +22,8 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
 
